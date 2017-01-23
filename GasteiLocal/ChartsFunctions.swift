cc8d35345f66862d751845ff10c3332c27e8019b
Andre Machado Parente <andremachadoparente@Andres-MacBook-Pro.local>
2017-01-23 16:16:16 -0200

    vamo que vamo

---
+++ GasteiLocal/ChartsFunctions.swift
@@ -0,0 +1,10 @@
+//
+//  ChartsFunctions.swift
+//  GasteiLocal
+//
+//  Created by Andre Machado Parente on 23/01/17.
+//  Copyright © 2017 Andre Machado Parente. All rights reserved.
+//
+
+import Foundation
+
--- GasteiLocal/GraficoViewController.swift
+++ GasteiLocal/GraficoViewController.swift
@@ -58,6 +58,8 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
     var vetorFinalCatMes: [String] = []
     var vetorFinalGastosMes: [Double] = []
     
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,8 +73,7 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
          view.backgroundColor = corAmarela
          }
          */
-        if (evermelha)
-        {
+        if (evermelha) {
             //view.backgroundColor = UIColor(patternImage: UIImage(named: "background_red.png")!)
             self.background_image.image = UIImage(named: "background_red.png")
         }
@@ -88,6 +89,118 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
         chartView.delegate = self
         chartView.backgroundColor = UIColor(white: 1, alpha: 0)
         chartView.holeColor = UIColor(white: 1, alpha: 0)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //executar = false
+        total = 0.0
+        
+        printaLimite(userLogged)
+        
+        if(userLogged.gastos.count == 0) {
+            
+            //NO DATA TEXT OCORRE QUANDO NAO TEM DADOS NO GRAFICO
+            chartView.clear()
+            
+            chartView.noDataText = "Você não possui nenhum gasto!"
+            //     chartView.infoTextColor = UIColor.white
+            //   chartView.infoFont = UIFont(name: "Tsukushi A Round Gothic", size: 16)
+            chartView.delegate = self
+            chartView.animate(xAxisDuration: 1)
+            totalLabel.isHidden = true
+            
+        }
+        else {
+            
+            let year = (Calendar(identifier: Calendar.Identifier.gregorian) as NSCalendar).component(.year, from: Date())
+            let month = (Calendar(identifier: Calendar.Identifier.gregorian) as NSCalendar).component(.month, from: Date())
+            
+            for gasto in userLogged.getGastosMes(month, ano: year){
+                total = total + gasto.value
+            }
+            print("--------- TOTAL DE TODOS OS GASTOS DO USUARIO DO Mes:  ", total)
+            (vetorFinal,vetorFinalCat) = organizaVetoresMes(userLogged,gastosMes:
+                userLogged.getGastosUltimoMês())
+            setPieChart(vetorFinalCat, values: vetorFinal)
+            
+        }
+        if (evermelha)
+        {
+            //view.backgroundColor = UIColor(patternImage: UIImage(named: "background_red.png")!)
+            self.background_image.image = UIImage(named: "background_red.png")
+            
+        }
+        if eazul{
+            //view.backgroundColor = UIColor(patternImage: UIImage(named: "background_blue.png")!)
+            self.background_image.image = UIImage(named: "background_blue.png")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        pickermesano.isHidden = true
+        dataMesTextField.text = ""
+    }
+
+    //FUNCAO QUE SETTA TODO O GRAFICO
+    func setPieChart(_ dataPoints: [String], values: [Double]) {
+        
+        
+        if(values.count == 0) {
+            chartView.clear()
+        }
+        else {
+            
+            chartView.centerText = "TESTEZAO"
+            var dataEntries: [ChartDataEntry] = []
+            for i in 0..<values.count {
+                let dataEntry = ChartDataEntry(x: Double(i), y: values[i].roundToPlaces(2))
+                print("x:  ", values[i])
+                print("y:  ", Double(i))
+                dataEntries.append(dataEntry)
+            }
+            let chartDataSet = PieChartDataSet(values: dataEntries, label: "Categorias")
+            chartDataSet.entryLabelColor = .white
+            
+            var r, g, b: CGFloat!
+            
+            chartDataSet.colors.removeAll()
+            
+            for i in 0...dataPoints.count {
+                
+                if (eazul) {
+                    r = CGFloat(Double(i)*10 + 64)
+                    g = CGFloat(Double(i)*20 + 138)
+                    b = CGFloat(Double(i)*30 + 202)
+                }
+                    
+                else if (evermelha) {
+                    r = CGFloat(Double(i)*10 + 146)
+                    g = CGFloat(Double(i)*20 + 16)
+                    b = CGFloat(Double(i)*30 + 16)
+                }
+                
+                chartDataSet.colors.append(NSUIColor(red: r/255, green: g/255, blue: b/255, alpha: 1))
+            }
+            let chartData = PieChartData(dataSet: chartDataSet)
+            
+            print(chartData.dataSets.count, chartData.dataSets)
+            for i in 0..<chartData.dataSets.count {
+                print(chartData.dataSets[i].label!)
+            }
+            chartView.data = chartData
+
+            totalLabel.text = "Total desse mês: R$ "+String(total)
+            totalLabel.isHidden = false
+        }
+        
+        
+    }
+    
+    // FUNCAO CHAMADA QUANDO CLICAMOS EM CIMA DE UM PEDACO DA PIZZA
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: Highlight) {
+ //       print("\(entry.value) in \(userLogged.categories[entry.index])")
+        print(chartView.xAxis.labelCount)
+        
     }
     
     //Funcao para organizar o grafico
@@ -138,8 +251,6 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
     
     func organizaVetoresMes(_ usuario: User, gastosMes: [Gasto]) -> ([Double],[String]) {
         
-        
-        
         var vetCatAux: [String] = []
         var vetValAux: [Double] = []
         for i in 0..<gastosMes.count {
@@ -161,10 +272,7 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
                 }
             }
         }
-        
         return (vetValAux,vetCatAux)
-        
-        
     }
     
     
@@ -179,69 +287,12 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
         else {
             //NO DATA TEXT OCORRE QUANDO NAO TEM DADOS NO GRAFICO
             chartView.noDataText = "Você não possui nenhum gasto!"
-           // chartView.infoTextColor = UIColor.white
-           // chartView.infoFont = UIFont(name: "Tsukushi A Round Gothic", size: 16)
+            // chartView.infoTextColor = UIColor.white
+            // chartView.infoFont = UIFont(name: "Tsukushi A Round Gothic", size: 16)
             chartView.delegate = self
             chartView.animate(xAxisDuration: 1)
             totalLabel.isHidden = true
         }
-    }
-    
-    
-    //FUNCAO QUE SETTA TODO O GRAFICO
-    func setPieChart(_ dataPoints: [String], values: [Double]) {
-        
-        
-        if(values.count == 0) {
-            chartView.clear()
-        }
-        else {
-            
-            
-            var dataEntries: [ChartDataEntry] = []
-           // chartView.descriptionText = ""
-            //ESSE FOR PREENCHE O VETOR DE ENTRADA DE DADOS, PRA CADA INDEX,
-            for i in 0..<values.count {
-                let dataEntry = ChartDataEntry(x: values[i].roundToPlaces(2), y: Double(i))
-                dataEntries.append(dataEntry)
-            }
-            
-            //ISSO EU NAO ENTENDI MUITO BEM MAS FUNCIONA
-           let chartDataSet = PieChartDataSet(values: dataEntries, label: "")
-            
-            var r, g, b: CGFloat!
-            
-            chartDataSet.colors.removeAll()
-            
-            for i in 0...userLogged.categories.count {
-                
-                if (eazul) {
-                    r = CGFloat(Double(i)*10 + 64)
-                    g = CGFloat(Double(i)*20 + 138)
-                    b = CGFloat(Double(i)*30 + 202)
-                }
-                    
-                else if (evermelha) {
-                    r = CGFloat(Double(i)*10 + 146)
-                    g = CGFloat(Double(i)*20 + 16)
-                    b = CGFloat(Double(i)*30 + 16)
-                }
-                
-                chartDataSet.colors.append(NSUIColor(red: r/255, green: g/255, blue: b/255, alpha: 1))
-            }
-            
-         //   let chartData = PieChartData(xVals: dataPoints, dataSet: chartDataSet)
-         //   chartView.data = chartData
-            totalLabel.text = "Total desse mês: R$ "+String(total)
-            totalLabel.isHidden = false
-        }
-        
-        
-    }
-    
-    // FUNCAO CHAMADA QUANDO CLICAMOS EM CIMA DE UM PEDACO DA PIZZA
-    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: Highlight) {
- //       print("\(entry.value) in \(userLogged.categories[entry.index])")
     }
     
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
@@ -254,54 +305,6 @@ class GraficoViewController: UIViewController,ChartViewDelegate,UITextFieldDeleg
             pickermesano.isHidden = true
         }
         return true
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        //executar = false
-        total = 0.0
-        
-        printaLimite(userLogged)
-        
-        if(userLogged.gastos.count == 0) {
-            
-            //NO DATA TEXT OCORRE QUANDO NAO TEM DADOS NO GRAFICO
-            chartView.clear()
-            
-            chartView.noDataText = "Você não possui nenhum gasto!"
-       //     chartView.infoTextColor = UIColor.white
-         //   chartView.infoFont = UIFont(name: "Tsukushi A Round Gothic", size: 16)
-            chartView.delegate = self
-            chartView.animate(xAxisDuration: 1)
-            totalLabel.isHidden = true
-            
-        }
-        else {
-            
-            let year = (Calendar(identifier: Calendar.Identifier.gregorian) as NSCalendar).component(.year, from: Date())
-            let month = (Calendar(identifier: Calendar.Identifier.gregorian) as NSCalendar).component(.month, from: Date())
-            
-            for gasto in userLogged.getGastosMes(month, ano: year){
-                total = total + gasto.value
-            }
-            print("--------- TOTAL DE TODOS OS GASTOS DO USUARIO DO Mes:  ", total)
-            (vetorFinal,vetorFinalCat) = organizaVetoresMes(userLogged,gastosMes:
-                userLogged.getGastosUltimoMês())
-            setPieChart(vetorFinalCat, values: vetorFinal)
-            
-        }
-        if (evermelha)
-        {
-            //view.backgroundColor = UIColor(patternImage: UIImage(named: "background_red.png")!)
-            self.background_image.image = UIImage(named: "background_red.png")
-            
-        }
-        if eazul{
-            //view.backgroundColor = UIColor(patternImage: UIImage(named: "background_blue.png")!)
-            self.background_image.image = UIImage(named: "background_blue.png")
-        }
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        pickermesano.isHidden = true
-        dataMesTextField.text = ""
     }
 }
 
