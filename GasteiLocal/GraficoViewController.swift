75a7c3631c6b027392f975908ff27e161ccac6ac
Andre Machado Parente <andremachadoparente@Andres-MacBook-Pro.local>
2017-01-23 13:33:58 -0200

    sem charts nenhum

--- GasteiLocal/GraficoViewController.swift
+++ GasteiLocal/GraficoViewController.swift
@@ -8,6 +8,7 @@
 //
 import UIKit
 import Charts
+
 // FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
 // Consider refactoring the code to use the non-optional operators.
 fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
